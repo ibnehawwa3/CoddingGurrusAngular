5105295523d696130e74fc5e2a0ad7294fdf127a
noornabi <noornabigtk@gmail.com>
2024-02-18 16:55:25 +0500

    bottom navbar issue resolved

--- src/app/include/header/header.component.ts
+++ src/app/include/header/header.component.ts
@@ -1,10 +1,10 @@
-import { Component,EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { CustomizeService } from '../../customize/customize.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   @Output() nightModeChanged = new EventEmitter<boolean>();
@@ -20,17 +20,16 @@ export class HeaderComponent {
     // localStorage.setItem('nightMode', JSON.stringify(this.isNightMode));
     this.dataService.changeMessage(this.isNightMode);
   }
-ngOnInit() {
-  this.toggleNightMode()
-}
+  ngOnInit() {
+    this.toggleNightMode();
+  }
 
-// id:any = 'grabbedblocks';
-// tapChange(ids:any){
-// this.id = ids;
-// this.dataService.changeMessage(this.id);
-// console.log(this.id)
-// }
-
+  // id:any = 'grabbedblocks';
+  // tapChange(ids:any){
+  // this.id = ids;
+  // this.dataService.changeMessage(this.id);
+  // console.log(this.id)
+  // }
 
   customOptions: OwlOptions = {
     loop: true,
@@ -38,47 +37,42 @@ ngOnInit() {
     rtl: true,
     touchDrag: false,
     pullDrag: false,
-    margin: 15,
     dots: false,
-    items: 100,
-    autoWidth:true,
-    lazyLoad: true,
+    items: 42, // Default number of items
+    autoWidth: false,
+    lazyLoad: false,
     navSpeed: 700,
-    navText: ['<i class="fa-solid fa-chevron-left"></i>', '<i class="fa-solid fa-chevron-right"></i>'],
+    navText: [
+      '<i class="fa-solid fa-chevron-left"></i>',
+      '<i class="fa-solid fa-chevron-right"></i>',
+    ],
+
     responsive: {
       0: {
-        items: 2,
+        items: 10,
       },
       400: {
-        items: 3,
+        items: 20,
       },
       740: {
-        items: 6,
-        autoWidth:false
+        items: 35,
       },
       940: {
         items: 40,
-        autoWidth:false
       },
       1300: {
-        items: 30,
-        autoWidth:true,
-        margin: 10,
-      }
+        items: 40,
+      },
     },
-    nav: true
-  }
-show_menus:boolean=false;
-  mobile_menus(){
+
+    nav: true,
+  };
+
+  show_menus: boolean = false;
+  mobile_menus() {
     this.show_menus = !this.show_menus;
   }
-  close_btn(){
+  close_btn() {
     this.show_menus = false;
   }
-
-
-
-
-
-
 }
