a03bece6ce5572e1546adb2783c736c86b6c4058
code-sekhar <75156986+code-sekhar@users.noreply.github.com>
2024-02-26 02:20:50 +0530

    header and copy issue fixed

--- src/app/common-component/common-editor/common-editor.component.ts
+++ src/app/common-component/common-editor/common-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CustomizeService } from '../../customize/customize.service';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'common-editor',
@@ -9,9 +10,10 @@ import { CustomizeService } from '../../customize/customize.service';
 export class CommonEditorComponent {
   title = 'the-coding-gurus';
   isNightMode = false;
-  constructor(private dataService: CustomizeService) {
+  constructor(private dataService: CustomizeService,private _clipboardService: ClipboardService) {
 
   }
+
   // ngOnInit() {
   //   let storedNightMode = localStorage.getItem('nightMode');
   //   this.isNightMode = storedNightMode ? JSON.parse(storedNightMode) : false;
@@ -23,4 +25,40 @@ export class CommonEditorComponent {
       console.log(this.isNightMode)
     });
   }
+
+  // copyText(){
+  //   let tooltip:any = document.getElementsByClassName("copy_tool_tip");
+  //   tooltip[1].innerText="Copied!";
+  //   let text_copy:any = document.getElementsByClassName("copy_txt");
+  //   let element = text_copy[1].innerText;
+  //   //this.clipboardService.copy(textToCopy);
+  //   this._clipboardService.copy(element);
+
+  // }
+  copyText() {
+    // Find all elements with class "copy_tool_tip"
+    let tooltips: HTMLCollectionOf<Element> = document.getElementsByClassName("copy_tool_tip");
+
+    // Iterate through each element with class "copy_tool_tip"
+    for (let i = 0; i < tooltips.length; i++) {
+        let tooltip = tooltips[i] as HTMLElement;
+
+        // Update the inner text of the tooltip
+        tooltip.innerText = "Copied!";
+    }
+
+    // Find all elements with class "copy_txt"
+    let textCopies: HTMLCollectionOf<Element> = document.getElementsByClassName("copy_txt");
+
+    // Iterate through each element with class "copy_txt"
+    for (let i = 0; i < textCopies.length; i++) {
+        let textCopy = textCopies[i] as HTMLElement;
+
+        // Get the text content of the element
+        let elementText = textCopy.innerText;
+
+        // Copy the text content to the clipboard
+        this._clipboardService.copy(elementText);
+    }
+}
 }
